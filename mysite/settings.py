498242c72768989b893ba48e708d6e53f6752a59
viragekaktus <danylo_graj@ukr.net>
2018-05-28 18:26:58 +0300

    allowed drsaldon.pythonanywhere.com

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f2b*xz(+rkm3t^xku=few=qb589p#v_f)*=mc+ll1j)^*9u#55'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [drsaldon.pythonanywhere.com]
 
 
 # Application definition
